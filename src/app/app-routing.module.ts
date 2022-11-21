bb9260cd040b295ed0935f1e1cca839e88bfd786
ealcca <edual986@gmail.com>
2022-11-21 17:21:39 -0300

    configure routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { CarsComponent } from './cars/cars.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',redirectTo: 'cars', pathMatch: 'full'},
+  { path: 'cars', component:CarsComponent},
+  { path: 'about-us',component:AboutUsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
