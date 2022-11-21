84185dd134ad08c4a7b6b8d561503b6c4f32c43c
ealcca <edual986@gmail.com>
2022-11-21 16:49:33 -0300

    edit component cars

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-cars',
+  selector: 'cars',
   templateUrl: './cars.component.html',
   styleUrls: ['./cars.component.scss']
 })
