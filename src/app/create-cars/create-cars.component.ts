09c427db692b7908aa908c72c9df3341cc45aee9
ealcca <edual986@gmail.com>
2022-11-23 23:17:14 -0300

    new component for add new car

--- src/app/create-cars/create-cars.component.ts
+++ src/app/create-cars/create-cars.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { CarDataService } from '../car-data.service';
+import { Car } from '../car-list/car';
 
 @Component({
-  selector: 'app-create-cars',
+  selector: 'create-cars',
   templateUrl: './create-cars.component.html',
   styleUrls: ['./create-cars.component.scss']
 })
 export class CreateCarsComponent {
 
+  carForm = new FormGroup({
+    brandname: new FormControl(''),
+    model: new FormControl(''),
+    year: new FormControl(''),
+    color: new FormControl(''),
+    price: new FormControl(''),
+    image: new FormControl('asset/fordk.jpg'),
+  }) 
+
+  constructor(private carsDataService:CarDataService) { }
+
+  createCar() {
+    /* let car : Car = {
+      'brandname':'zafari',
+      'model':'fere',
+      'year':2121,
+      'color':'red',
+      'price':2000,
+      'image':'assets/fordk.jpg',
+    }
+  this.carsDataService.create(car); */
+ 
+  //console.log(this.carForm.value);
+
+  this.carsDataService.create(this.carForm.value);
+  }
 }
+
