83cab57e1cd4d23af8091e043ace04f81cb9c037
ealcca <edual986@gmail.com>
2023-11-22 01:42:20 -0300

    Agrego data binding para el navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dealership';
+  logo = 'GAMA MULTIMARCAS';
+  imglogo = "assets/logo.jpg";
   year = new Date().getFullYear();
 }
