0d944bfc50be4a9f5c0fa5420f73683b86b82a58
ealcca <edual986@gmail.com>
2024-10-17 16:56:53 -0300

    elimino codigo comentado

--- src/app/brand-data.service.spec.ts
+++ src/app/brand-data.service.spec.ts
@@ -6,22 +6,15 @@ import { of } from 'rxjs';
 
 describe('BrandDataService', () => {
   let service: BrandDataService;
-  //const httpClientSpy = jasmine.createSpyObj('HttpClient',['get','post']);
   let httpTestingController: HttpTestingController;
-
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      //providers: [{ provide: HttpClient,useValue: httpClientSpy}]
-      //imports: [HttpClientTestingModule],
       imports: [HttpClientTestingModule],
     });
     service = TestBed.inject(BrandDataService);
     httpTestingController = TestBed.inject(HttpTestingController);
   });
-
-  //httpClientSpy.post.and.returnValue(of({ status:200, data:{}}));
-  //httpClientSpy.get.and.returnValue(of({ status:200, data:{}}));
 
   it('should be created', () => {
     expect(service).toBeTruthy();
