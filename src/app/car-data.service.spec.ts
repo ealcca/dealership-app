2ff9f477b1de05591aa89f006d66c02e5ac1904c
ealcca <edual986@gmail.com>
2024-10-17 16:55:01 -0300

    correccion de test sobre service de car-data

--- src/app/car-data.service.spec.ts
+++ src/app/car-data.service.spec.ts
@@ -1,13 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CarDataService } from './car-data.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('CarDataService', () => {
   let service: CarDataService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(CarDataService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
